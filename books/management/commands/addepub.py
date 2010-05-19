fa54755067c5473dc5547eb121c76fbb6cc1856e
Kushal Das <kushal@fedoraproject.org>
2010-05-20 01:25:37 +0530

    Added new command addepub , which will take an absolute dirpath as argument and recursivly scans for all EPUBs and add them in the db

---
+++ books/management/commands/addepub.py
@@ -0,0 +1,64 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.files.storage import default_storage
+from django.core.files import File
+
+import os
+import csv
+import glob
+
+from books.models import *
+from books.epub import *
+from books.langlist import *
+
+import settings
+
+
+
+def get_epubs(path):
+    """Returns a list of EPUB(s)"""
+    epub_list = []
+
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.lower().endswith('.epub'):
+                epub_list.append(os.path.join(root,name))
+
+    return epub_list
+
+
+class Command(BaseCommand):
+    help = "Adds a book collection (via a directory full of EPUB fili(s))"
+    args = 'Absolute path to directory of EPUB files'
+
+    def handle(self, dirpath='', *args, **options):
+        if not os.path.exists(dirpath):
+            raise CommandError("%r is not a valid path" % dirpath)
+
+
+        if os.path.isdir(dirpath):
+            names = get_epubs(dirpath)
+            for name in names:
+                info = None
+                try:
+                    e = Epub(name)
+                    info = e.get_info()
+                except:
+                    print "%s is not a valid epub file" % name
+                    continue
+                lang = Language.objects.filter(code=info.language)
+                if not lang:
+                    for data in langs:
+                        if data[0] == info.language:
+                            lang = Language()
+                            lang.label = data[1]
+                            lang.save()
+                            break
+                else:
+                    lang = lang[0]
+                f = open(name)
+                book = Book(file=File(f), a_title = info.title, \
+                        a_author = info.creator, a_summary = info.summary, \
+                        a_rights = info.rights, dc_identifier = info.identifier['value'].strip('urn:uuid:'), \
+                        dc_issued = info.date)
+                book.save()
+
