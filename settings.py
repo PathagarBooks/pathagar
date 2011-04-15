3139a0638826c12cfe34c0dd840b74559f0989df
Manuel Quiñones <manuel.por.aca@gmail.com>
2011-04-15 12:27:58 -0300

    Comments in dev settings and remove boilerplate

--- settings.py
+++ settings.py
@@ -1,8 +1,14 @@
-# Django settings for pathagar project.
 
-# Get current directory to get media and templates while developing:
-import os
-CUR_DIR = u'' + os.path.dirname(__file__)
+"""
+This settings are for testing Pathagar with the Django development
+server.  It will use a SQLite database in the current directory and
+Pathagar will be available at http://127.0.0.1:8000 loopback address.
+
+For production, you should use a proper web server to deploy Django,
+serve static files, and setup a proper database.
+
+"""
+
 
 # Books settings:
 
@@ -10,6 +16,10 @@ BOOKS_PER_PAGE = 2 # Number of books shown per page in the OPDS
                    # catalogs and in the HTML pages.
 
 BOOKS_STATICS_VIA_DJANGO = True
+
+# Get current directory to get media and templates while developing:
+import os
+CUR_DIR = u'' + os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -27,51 +37,33 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/Chicago'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(CUR_DIR, 'static_media')
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = 'http://127.0.0.1:8000/static_media'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = '7ks@b7+gi^c4adff)6ka228#rd4f62v*g_dtmo*@i62k)qn=cs'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'pathagar.urls'
@@ -79,9 +71,6 @@ ROOT_URLCONF = 'pathagar.urls'
 INTERNAL_IPS = ('127.0.0.1',)
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
