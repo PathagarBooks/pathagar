f4a2d8d5e9cb7682afb7c6478a6f17d8ee8958a2
Seth Woodworth <seth@sethish.com>
2013-03-08 16:34:53 -0500

    add taggit to INSTALLED_APPS

--- settings.py
+++ settings.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'tagging',
+    'tagging', # TODO old
+    'taggit',
     'pathagar.books'
 )
