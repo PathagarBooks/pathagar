438b168463cf15bf0107866d443e7a381764d2a8
Aaron D Borden <adborden@a14n.net>
2017-02-16 08:06:53 -0800

    Add some tests for login required actions

--- books/tests.py
+++ books/tests.py
@@ -6,6 +6,8 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test import Client
+from books.models import Book, Status
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -21,3 +23,58 @@ Another way to test that 1 + 1 is equal to 2.
 True
 """}
 
+class LoginRequiredPagesTest(TestCase):
+    def setUp(self):
+        #TODO move this into a test fixture
+        published = Status(status="Published")
+        published.save()
+
+        book = Book(
+            a_title="Red Fish Two Fish One Fish Blue Fish",
+            a_author="Dr. Seuss",
+            a_status=published,
+        )
+        book.save()
+
+    def test_login_page_ok(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_book_requires_login(self):
+        #TODO this should end with a `/`
+        url = '/book/add'
+        response = self.client.get(url)
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
+
+        response = self.client.post(url, {"title": "Computer Vision"})
+        #TODO this should probably a 403
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
+
+    def test_edit_book_requires_login(self):
+        #TODO this should end with a `/`
+        url = '/book/1/edit'
+        response = self.client.get(url)
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
+
+        response = self.client.put(url, {"title": "Two Scoops of Django"})
+        #TODO this should probably a 403
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
+
+    def test_remove_book_requires_login(self):
+        #TODO this should end with a `/`
+        url = '/book/1/remove'
+        response = self.client.get(url)
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
+
+        response = self.client.delete(url, {"title": "Two Scoops of Django"})
+        #TODO this should probably a 403
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
+
+    def test_add_language_requires_login(self):
+        url = '/add/language/'
+        response = self.client.get(url)
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
+
+        response = self.client.post(url, {"title": "Two Scoops of Django"})
+        #TODO this should probably a 403
+        self.assertRedirects(response, '/accounts/login/?next=%s' % url, status_code=302)
