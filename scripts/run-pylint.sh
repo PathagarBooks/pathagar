83c49beaebfafb967d2b038b918f70416a768e4d
Michael Bonfils <murlock42@gmail.com>
2017-10-20 10:27:49 +0200

    Update .travis
    No need to use virtualenv with travis as each version already use a
    virtualenv instance.
    
    Update test-requirements and use python -m pylint syntax.
    
    Fix mispelled language entry

--- scripts/run-pylint.sh
+++ scripts/run-pylint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for file in $(git ls-files -- '*.py' | grep -vE 'deploy|fabfile'); do
+echo "========= PYTHON VERSION "$(python --version)
+for file in $(git ls-files -- '*.py'); do
     echo "==== $file"
-    pylint --rcfile=pylintrc --load-plugins pylint_django --score=no --reports=no --disable=R,C $file
+    python -m pylint --rcfile=pylintrc --load-plugins pylint_django --score=no --reports=no --disable=R,C $file
     RET=$? # pylint returns bitfield of found issues
     ERR=$(($RET&3)) # Fatal is 1 and Error is 2
     if [[ $ERR -ne 0 ]]; then
