f827cec848a49e4d790558b9c401484670c63568
Michael Bonfils <murlock42@gmail.com>
2017-10-29 14:48:19 +0100

    Add simple test for addepub cmd

--- books/tests.py
+++ books/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+from django.core.management import call_command, CommandError
+
 from books.epub import Epub
+from books.models import Book, Author
+
 
 class EpubTest(TestCase):
     def test_simple_import(self):
@@ -8,3 +12,28 @@ class EpubTest(TestCase):
         self.assertEqual(info.title, "The Dunwich Horror")
         self.assertEqual(info.creator, "H. P. Lovecraft")
         epub.close()
+
+
+class AddEpubTest(TestCase):
+    def test_01_import_commandline(self):
+        nb_book = len(Book.objects.all())
+        self.assertEqual(nb_book, 0)
+
+        args = ["examples/"]
+        opts = {}
+        call_command('addepub', *args, **opts)
+
+        nb_book = len(Book.objects.all())
+        self.assertEqual(nb_book, 1)
+
+        book = Book.objects.get(pk=1)
+        self.assertEqual(str(book.a_author), "H. P. Lovecraft")
+        self.assertEqual(str(book.a_title), "The Dunwich Horror")
+
+    def test_02_import_duplicated(self):
+        # try to import duplicated epub
+        args = ["examples/"]
+        opts = {}
+        call_command('addepub', *args, **opts)
+        self.assertRaises(CommandError, call_command,
+            ('addepub'), opts)
