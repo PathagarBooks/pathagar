28ba63609dbd31b0cfd7bc8b4d698f1b111bcbe7
Aaron D Borden <adborden@a14n.net>
2019-03-09 16:23:02 -0800

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pathagar',
+    version='0.8.1',
+    description='Pathagar is a simple bookserver serving OPDS feeds',
+    url='http://github.com/PathagarBooks/pathagar',
+    license='AGPL-3.0',
+    packages=['books', 'pathagar'],
+    zip_safe=False)
